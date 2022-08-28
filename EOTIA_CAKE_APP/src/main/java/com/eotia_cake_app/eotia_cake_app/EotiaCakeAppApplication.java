c6b26e3158ac8b52c0cbf72aab169a97612d466d
InflvxDev <seochoa@unicesar.edu.co>
2022-08-27 20:59:19 -0500

    Create code initial

---
+++ EOTIA_CAKE_APP/src/main/java/com/eotia_cake_app/eotia_cake_app/EotiaCakeAppApplication.java
@@ -0,0 +1,13 @@
+package com.eotia_cake_app.eotia_cake_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EotiaCakeAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EotiaCakeAppApplication.class, args);
+    }
+
+}
---
+++ EOTIA_CAKE_APP/src/test/java/com/eotia_cake_app/eotia_cake_app/EotiaCakeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eotia_cake_app.eotia_cake_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EotiaCakeAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
