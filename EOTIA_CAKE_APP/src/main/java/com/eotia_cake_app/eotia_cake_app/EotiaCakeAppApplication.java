56a88f361bf23254acb0078a57c4cd5211089bc0
InflvxDev <seochoa@unicesar.edu.co>
2022-10-08 19:51:12 -0500

    Create build war

--- EOTIA_CAKE_APP/src/main/java/com/eotia_cake_app/eotia_cake_app/EotiaCakeAppApplication.java
+++ EOTIA_CAKE_APP/src/main/java/com/eotia_cake_app/eotia_cake_app/EotiaCakeAppApplication.java
@@ -2,6 +2,8 @@ package com.eotia_cake_app.eotia_cake_app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
 
 @SpringBootApplication
 public class EotiaCakeAppApplication {
